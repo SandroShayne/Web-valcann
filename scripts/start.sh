025724e75be1c136fa8dcb20c08a6915a8611ec4
SandroShayne <sandro.dutra1@hotmail.com>
2023-07-19 22:33:37 -0300

    s

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,9 +3,15 @@ nomeApp="nomeApp"
 cd /appDeploy
 sudo npm install
 sudo npm cache clean --force
+sudo npm install -g n
+sudo n stable
 sudo npm audit fix --force
 sudo npm update
+sudo npm install --save-dev @babel/plugin-proposal-private-property-in-object
+
 sudo npm start
 sudo supervisorctl reread
 sudo supervisorctl update
 sudo supervisorctl start $nomeApp
+
+
