e817c73e8c8cf5171b481d1131a356d4eacff53c
SandroShayne <sandro.dutra1@hotmail.com>
2023-07-19 22:04:10 -0300

    st

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 nomeApp="nomeApp"
 cd /appDeploy
-
 sudo npm install
+sudo npm cache clean --force
+sudo npm audit fix --force
+sudo npm update
 sudo npm start
 sudo supervisorctl reread
 sudo supervisorctl update
