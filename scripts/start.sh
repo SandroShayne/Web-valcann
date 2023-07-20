0e7233f5e3d4d7466c6c9928191d6917f9916fed
SandroShayne <sandro.dutra1@hotmail.com>
2023-07-20 16:44:33 -0300

    s

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,7 +3,7 @@ nomeApp="nomeApp"
 cd /appDeploy
 sudo npm install
 
-sudo supervisord -c /etc/supervisord.conf
+#sudo supervisord -c /etc/supervisord.conf
 sudo supervisorctl reread
 sudo supervisorctl update
 sudo supervisorctl start $nomeApp
