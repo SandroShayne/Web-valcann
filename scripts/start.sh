507c20e70fb2d8c4c9fec5037456431b58cbfc30
SandroShayne <sandro.dutra1@hotmail.com>
2023-07-20 12:38:36 -0300

    s

--- scripts/start.sh
+++ scripts/start.sh
@@ -3,7 +3,7 @@ nomeApp="sandroApp"
 cd /appDeploy/src
 sudo npm install
 sudo npm start
-#sudo supervisord -c /etc/supervisord.conf
+sudo supervisord -c /etc/supervisord.conf
 sudo supervisorctl reread
 sudo supervisorctl update
 sudo supervisorctl start $sandroApp
