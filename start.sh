05ee7d03017b91e3939b4f8019294da4afab7e9e
SandroShayne <sandro.dutra1@hotmail.com>
2023-07-22 13:02:59 -0300

    s

---
+++ start.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+nomeApp="nomeApp"
+cd /appDeploy/src
+sudo npm install
+sudo npm start
+
+
+sudo supervisorctl reread
+sudo supervisorctl update
+sudo supervisorctl start $nomeApp
+
+
+
+
---
+++ stop.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+nomeApp="nomeApp"
+
+sudo supervisorctl stop $nomeApp
