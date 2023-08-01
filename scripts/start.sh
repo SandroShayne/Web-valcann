513960caa248aeb9cc97198af850b33968c6429c
Sandro Shayne dutra <39307787+SandroShayne@users.noreply.github.com>
2023-08-01 10:29:32 -0300

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-nomeApp="nomeApp"
-cd /appDeploy/src
-sudo npm install
-sudo nvm install v12
-sudo nvm use v12
-sudo npm start
+nomeApp="helloreact"
+#cd /home/ubuntu/Web-valcann
+#sudo npm install
+#sudo nvm install v12
+#sudo nvm use v12
+#sudo npm start
 
 
-sudo supervisorctl reread
-sudo supervisorctl update
+#sudo supervisorctl reread
+#sudo supervisorctl update
 sudo supervisorctl start $nomeApp
 
 
